93796a80da4908abd65179996e1b1218774adad7
WeiTianyue <e0968931@u.nus.edu>
2025-04-01 10:50:21 +0800

    Refine rule-based model

--- Models/rule-decision-tree.py
+++ Models/rule-decision-tree.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
 import os
-from collections import Counter
+from nltk.corpus import cmudict
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, classification_report
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Download nltk dependencies
+# import nltk
+# nltk.download('cmudict')
 
 # Load dataset
 CSV_FILE_PATH = os.path.join('Datasets', 'Sarcasm_Headlines_Dataset_v2.csv')
@@ -29,14 +34,21 @@ train_texts, val_texts, train_labels, val_labels = train_test_split(
     headlines, labels, test_size=0.2, random_state=RANDOM_SEED
 )
 
+# Lexicon of syllabus to detect rhymes
+pron_dict = cmudict.dict()
+
+# Get syllabus count for individual word
+def count_syllables(word):
+    return max([len(list(y for y in x if y[-1].isdigit())) for x in pron_dict.get(word.lower(), [[]])])
+
 def extract_features(texts):
-    
+    analyzer = SentimentIntensityAnalyzer()
+
     features = []
     for text in texts:
         text = text.lower()
         tokens = word_tokenize(text)
         tagged_tokens = pos_tag(tokens)
-        word_counts = Counter(tokens)
 
         # Punctuations
         num_exclamations = text.count("!")
@@ -48,10 +60,15 @@ def extract_features(texts):
         num_adverbs = sum(1 for _, tag in tagged_tokens if tag.startswith("RB"))
         num_verbs = sum(1 for _, tag in tagged_tokens if tag.startswith("VB"))
         num_nouns = sum(1 for _, tag in tagged_tokens if tag.startswith("NN"))
+
+         # Rhythmic patterns (syllables)
+        num_syllables = sum(count_syllables(word) for word in tokens if word.isalpha())
+        is_rythmic = num_syllables > 1
     
         features.append([
             num_adjectives, num_adverbs, num_verbs, num_nouns,
             num_exclamations, num_questions, num_quotes,
+            is_rythmic,
         ])
     return np.array(features)
 
